f638add853607b7cc059540def745844e3423533
Ralf Stemmer <ralf.stemmer@gmx.net>
2018-01-30 21:44:14 +0100

    Strip-option removed

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo -e "This program comes with \e[1;33mABSOLUTELY NO WARRANTY\e[0m."
 echo -e "This is free software, and you are welcome to redistribute it"
 echo -e "under certain conditions.\n"
 
-install -m 755 -v -s -g root -o root sterm4.py -D $INSTALLPATH/sterm
+install -m 755 -v -g root -o root sterm4.py -D $INSTALLPATH/sterm
 
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
